f1d49ff1c658e37b913bf070261d6e8e0962a4c4
sdymj84 <sdymj84@gmail.com>
2018-07-14 14:30:10 -0500

    complete coffee-house project for practice

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-let checkoutFormSubmit = document.getElementById("checkout_form_submit");
+function myMap() {
+    var myPosition = new google.maps.LatLng(38.884876, -94.651446);
 
-checkoutFormSubmit.addEventListener("click", function() {
-    
-});
+    var map = new google.maps.Map(document.querySelector("#store-location"), {
+        center: myPosition,
+        zoom: 15
+    });
+
+    var marker = new google.maps.Marker({
+        position: myPosition,
+        map: map,
+        title: "Joe's Coffee Store"
+    });
+}
